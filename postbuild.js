fd829582c5a781431775ea403ada411335b3056b
SandorStefan62 <sandor.stefanandrei02@gmail.com>
2024-06-19 22:29:52 +0300

    updated postbuild to es modules

--- postbuild.js
+++ postbuild.js
@@ -1,19 +1,33 @@
-const fs = require("fs");
-const path = require("path");
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const distDir = path.join(__dirname, 'dist', 'assets');
-const fileName = fs.readdirSync(distDir).find(file => file.endsWith('.js'));
 
-if (!fileName) {
-    console.error('Built javascript file not found.');
-    process.exit(1);
+async function updateFile() {
+    try {
+        const files = await fs.readdir(distDir);
+        const fileName = files.find(file => file.endsWith('.js'));
+
+        if (!fileName) {
+            console.error('Built javascript file not found');
+            process.exit(1);
+        }
+
+        const filePath = path.join(distDir, fileName);
+        let fileContent = await fs.readFile(filePath, 'utf8');
+
+        fileContent = fileContent.replace(/ei\./g, '');
+
+        await fs.writeFile(filePath, fileContent, 'utf8');
+
+        console.log(`Updated ${fileName}`);
+    } catch (error) {
+        console.error('Error updating file: ', error);
+    }
 }
 
-const filePath = path.join(distDir, fileName);
-let fileContent = fs.readFileSync(filePath, 'utf8');
-
-fileContent = fileContent.replace(/ei\./g, '');
-
-fs.writeFileSync(filePath, fileContent, 'utf8');
-
-console.log(`Updated ${fileName}`);
+updateFile();
