7d2316a348747525e39156637f0638b584be1618
SandorStefan62 <sandor.stefanandrei02@gmail.com>
2024-06-19 22:39:49 +0300

    updated postbuild to es modules

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@mediapipe/holistic']
+  },
   build: {
+    rollupOptions: {
+      output: {
+        globals: {
+          '@mediapipe/holistic': 'Holistic'
+        }
+      }
+    },
     minify: false
   }
 })
